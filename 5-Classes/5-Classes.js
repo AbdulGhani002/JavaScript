a6c19bc383f8b471a5f0485cd2425b5ff1fa2c6f
Muhammad Abdul Ghani Qureshi <itsaghani@gmail.com>
2022-12-04 10:24:56 +0500

    Classes are Updated

--- 5-Classes/5-Classes.js
+++ 5-Classes/5-Classes.js
@@ -2,10 +2,23 @@ class Job {
   //First letter of class-name should be capitalized.
   constructor(jobTitle, place, salary) {
     this.title = jobTitle;
-    this.place = place;
+    this.location = place;
     this.salary = salary;
+  }
+  // We can define methods for our classes like Date has a method of toISOString().
+  describe() {
+    return `I am a ${this.title}, I work in ${this.location}, I earns $${this.salary} per anum.`;
   }
 }
 
 const developer = new Job("Web Developer", "Pakistan", 60000);
-const cok = new Job("Cook", "New York", 600000);
+
+const cook = new Job("Cook", "New York", 600000);
+
+console.log(developer);
+
+console.log(cook);
+
+console.log(developer.describe());
+
+console.log(cook.describe());
